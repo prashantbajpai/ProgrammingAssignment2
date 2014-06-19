f9c756f42549d599bdfde09c3435d4d676f81024
prashantbajpai <prashantbajpai112@gmail.com>
2014-06-19 19:45:59 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These pair of functions catches the inverse of an invertible matrix
+## 
 
-## Write a short comment describing this function
+## makeCatchMatrix creates a list of four functions
+## set function sets the input matrix which is given as an argument to makeCatchMatrix function. It doesn't do anything. 
+#  Even if you remove this function your overall result will not change
+## get function takes the matrix which was set by set function 
+## setinverse stores the inverse in the enclosing enviroment i.e. the enviroment in which makeCatchMatrix was defined
+## getinverse gets the inverse value from the setinverse function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCatchMatrix<-function(x=matrix()){
+        m <- matrix(NA,nrow(x),ncol(x))
+        set <- function(y) {
+                x <<- y
+                
+                m <<- matrix(NA,nrow(x),ncol(x))
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function performs two main operations
+## First it checks if inverse matrix(m) is null or not. If its null then compute the inverse and assign it to m.
+## If inverse matrix is not null then recycle that matrix instead of computing inverse again.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(length(which(complete.cases(m)))!=0) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
+
